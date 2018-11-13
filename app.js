bedcc9d7f754b6480d5076813e909759b44240b8
FXGSI <32699775+FXGSI@users.noreply.github.com>
2018-11-13 20:07:57 +0800

    解决跨域问题

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ app.use(session({
 }));
 //req.session
 /*使用路由器来管理路由*/
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 
 //app.use("/index",index);
 app.use("/details",details);
